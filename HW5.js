a5016ed2c960ed512fbd8d3b19407ff0c08e5ad9
clarjohn <59753565+clarjohn@users.noreply.github.com>
2020-02-29 16:26:02 -0800

    f1

---
+++ HW5.js
@@ -0,0 +1,47 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 7034);
+
+
+app.get('/', function(reg,res){
+     var inputlist = [];
+     for(var i in res.query){
+         inputlist.push() = {"v1":i,"v2":res.query[i]};
+     }
+     var Obj = {};
+     Obj.item = inputlist;
+     Obj.met = "GET";
+    res.render('home',Obj);
+
+});
+
+
+
+
+
+
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+  });
+  
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+  });
+  
+  app.listen(app.get('port'), function(){
+    console.log('Express started on Port:' + app.get('port') + '; press Ctrl-C to terminate.');
+  });
